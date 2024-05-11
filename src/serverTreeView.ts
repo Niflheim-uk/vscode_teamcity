a76d55e6c94c9e3f0cd35e3a97e5787cc8daec87
Niflheim-uk <loom-narwhal-0i@icloud.com>
2024-05-11 20:13:58 +0100

    Added refresh function

--- src/serverTreeView.ts
+++ src/serverTreeView.ts
@@ -4,7 +4,13 @@ import { ServerTreeItem } from './serverTreeItem';
 export class ServerTreeView implements vscode.TreeDataProvider<ServerTreeItem> {
   constructor() {
   }
-
+  private _onDidChangeTreeData: vscode.EventEmitter<ServerTreeItem | undefined | null | void> = new vscode.EventEmitter<ServerTreeItem | undefined | null | void>();
+  readonly onDidChangeTreeData: vscode.Event<ServerTreeItem | undefined | null | void> = this._onDidChangeTreeData.event;
+  
+  refresh(item?:ServerTreeItem): void {
+    this._onDidChangeTreeData.fire(item);
+  }
+  
   getTreeItem(element:ServerTreeItem): vscode.TreeItem {
     return element;
   }
